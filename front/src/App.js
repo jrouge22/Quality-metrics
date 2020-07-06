ff2bdb042a1397c356c5ac4141d026b2af29d836
Julien Rougé <rouge.julien@gmail.com>
2020-07-07 00:21:22 +0200

    Initialisation Front avec React Admin

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { HydraAdmin, fetchHydra, hydraDataProvider } from "@api-platform/admin";
+import { parseHydraDocumentation } from "@api-platform/api-doc-parser";
+
+const entrypoint = process.env.REACT_APP_API_ENTRYPOINT;
+
+const dataProvider = hydraDataProvider(
+    entrypoint,
+    fetchHydra,
+    parseHydraDocumentation,
+    true // useEmbedded parameter
+);
+
+export default () => (
+    <HydraAdmin
+        dataProvider={ dataProvider }
+        entrypoint={ entrypoint }
+    />
+);
