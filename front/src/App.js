915b7892782234ff8d7c7c41b38706bb4a275e34
Julien Rougé <rouge.julien@gmail.com>
2020-07-08 14:02:22 +0200

    React Admin : Mise en place de l'affichage admin

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { HydraAdmin, fetchHydra, hydraDataProvider } from "@api-platform/admin";
+import { HydraAdmin, fetchHydra, hydraDataProvider, ResourceGuesser } from "@api-platform/admin";
 import { parseHydraDocumentation } from "@api-platform/api-doc-parser";
+import { MetricList } from './metrics';
+import { VersionList } from './versions';
+import { TechnoList, TechnoShow } from './technos';
+import Dashboard from './dashboard';
+
+// TODO : Recherche d'autres icones
+import metricIcon from '@material-ui/icons/Book';
+import versionIcon from '@material-ui/icons/Group';
+import technoIcon from '@material-ui/icons/Group';
 
 const entrypoint = process.env.REACT_APP_API_ENTRYPOINT;
 
@@ -8,12 +17,18 @@ const dataProvider = hydraDataProvider(
     entrypoint,
     fetchHydra,
     parseHydraDocumentation,
-    true // useEmbedded parameter
+    true
 );
 
 export default () => (
-    <HydraAdmin
+      <HydraAdmin
+        dashboard={ Dashboard }
         dataProvider={ dataProvider }
         entrypoint={ entrypoint }
-    />
+    >
+        <ResourceGuesser name="metrics" list={MetricList} icon={metricIcon} />
+        <ResourceGuesser name="technos" list={TechnoList} show={TechnoShow} icon={technoIcon} />
+        <ResourceGuesser name="versions" list={VersionList} icon={versionIcon} />
+
+    </HydraAdmin>
 );
---
+++ front/src/metrics.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextField, CardActions, List, Datagrid } from 'react-admin';
+
+export const MetricList = (props) => (
+    <List {...props}>
+        <Datagrid>
+                <TextField source="name" label="Métrique" />
+                <TextField source="levelOk" label="Palier minimum" />
+                <TextField source="levelNice" label="Palier confort" />
+        </Datagrid>
+    </List>
+);
